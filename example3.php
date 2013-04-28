521c0b5980e0e298fcbff79efbce56ce782d8187
Stephan Schmitz <eyecatchup@gmail.com>
2013-04-28 14:08:10 +0200

    Updated pastebin example

--- example3.php
+++ example3.php
@@ -1,11 +1,7 @@
 <?php
 $WD = __DIR__ . DIRECTORY_SEPARATOR;
-
 require_once $WD . 'CC_Service.php';
 try {
-  // name for the final script file
-  $filename   = $WD . sprintf('scripts.min.%s.js', date('Ymd'));
-
   // define local JavaScript resource(s) to be compiled (use absolute paths!)
   $localFiles = array(
     '/var/www/project/static/js/script1.js',
@@ -16,13 +12,17 @@ try {
   // create new instance
   $cc = new CC_Service();
 
-  // merge and upload the local files (to pastebin) to be available for the compiler
+  // merge local files and create a temorarily public available resource
   $code_url = $cc->getTempUrl($localFiles);
 
   if ($code_url) {
     $cc->addScript($code_url);
-    file_put_contents($filename, $cc->postService());
-    print "Saved compiled script(s) to {$filename}\n";
+	
+    // save the compiled script(s) to a local file
+    $filename = sprintf('scripts.min.%s.js', date('Ymd'));
+    file_put_contents($WD . $filename, $cc->postService());
+
+    print "Saved compiled script(s) to {$WD}{$filename}\n";
   }
 }
 catch (CC_Service_Exception $e) {
